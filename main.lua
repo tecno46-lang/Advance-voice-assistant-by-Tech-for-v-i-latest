b93086397a300934c75b7a286dd66e250447a00d
tecno46-lang <tecno46ch@gmail.com>
2026-03-06 14:01:46 +0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -146,7 +146,7 @@ function downloadAndInstallUpdate()
 end
 
 local CONSTANTS = {
-    VERSION = "1.3",
+    VERSION = "1.2",
     PREF_NAME = "Hanzla_Final_Safety_V7_Enhanced",
     DELAYS = {
         SUPER_FAST = 80,
